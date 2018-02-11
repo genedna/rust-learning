36defe3e8e455d8d515c225e2dbea8a4d2b7b0a2
Eli Ma <eli@patch.sh>
2018-02-11 19:52:16 +0800

    Refactoring cli struct
    Signed-off-by: Eli Ma <eli@patch.sh>

--- cloud/do-operation/src/main.rs
+++ cloud/do-operation/src/main.rs
@@ -2,39 +2,50 @@
 
 extern crate clap;
 
-use std::process;
-use clap::{App, SubCommand};
+use clap::{App, Arg, SubCommand};
 
 fn main() {
-    let matches = App::new(crate_name!())
+  let matches = App::new(crate_name!())
     .version(crate_version!())
     .author(crate_authors!())
     .about(crate_description!())
-    .args_from_usage("-k,   --key=[API_KEY] 'Sets a Digitalocean API key'
-                      -c... --config=[FILE] 'Sets a custom config file'
+    .arg(
+      Arg::with_name("key")
+      .help("Digitalocean API key")
+      .short("k")
+      .long("key")
+      .takes_value(true)
+      .required(true))
+    .args_from_usage("-c,   --config=[FILE] 'Sets a custom config file'
                       -v... 'Sets the level of verbosity'
                       -d... 'Turn debugging information on'")
-    .subcommand(SubCommand::with_name("droplet")
-      .about("Digitalocean droplet operations"))
-    .subcommand(SubCommand::with_name("loadbalancer")
-      .about("Digitalocean loadbalancer operations"))
+    .subcommand(
+      SubCommand::with_name("droplet")
+        .about("Digitalocean droplet operations")
+        .subcommand(
+          SubCommand::with_name("list")
+            .about("List all digitalocean droplets")
+        )
+        .subcommand(
+          SubCommand::with_name("create")
+            .about("Create a droplet in Digitalocean")
+        )
+        .subcommand(
+          SubCommand::with_name("delete")
+            .about("Delete a droplet in Digitalocean")  
+        ))
+    .subcommand(
+      SubCommand::with_name("loadbalancer")
+        .about("Digitalocean loadbalancer operations"))
     .get_matches();
 
   let mut _api_key = String::new();
 
   if let Some(key) = matches.value_of("key") {
     _api_key = key.to_string();
-  } else {
-    println!("Operate digitalocean API needs ");
-    process::exit(-1);
   }
 
-  match matches.subcommand_name() {
-    Some("droplet")      => println!("management digitalocean droplet"),
-    Some("loadbalancer") => println!("management digitalocean loadbalancer"),
-    None                 => println!("No subcommand was used"),
-    _                    => unreachable!(),
+  if let Some(ref _droplet) = matches.subcommand_matches("droplet") {
+    println!("{:?}", _api_key);
   }
-
-  process::exit(0);
 }
