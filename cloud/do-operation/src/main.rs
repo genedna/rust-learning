109c4b3e6c83233dd65dfaa2393090f651ad3a2f
Eli Ma <eli@patch.sh>
2018-02-18 21:49:35 +0800

    new command struct for droplet and load balancer
    Signed-off-by: Eli Ma <eli@patch.sh>

--- cloud/do-operation/src/main.rs
+++ cloud/do-operation/src/main.rs
@@ -29,6 +29,12 @@ fn main() {
         .subcommand(
           SubCommand::with_name("create")
             .about("Create a droplet in Digitalocean")
+            .arg(Arg::with_name("cpu")
+              .help("Droplet CPU size")
+              .short("c")
+              .long("cpu")
+              .takes_value(true)
+              .required(true))
         )
         .subcommand(
           SubCommand::with_name("delete")
@@ -39,13 +45,67 @@ fn main() {
         .about("Digitalocean loadbalancer operations"))
     .get_matches();
 
-  let mut _api_key = String::new();
+  let mut api_key = String::new();
 
   if let Some(key) = matches.value_of("key") {
-    _api_key = key.to_string();
+    api_key = key.to_string();
   }
 
-  if let Some(ref _droplet) = matches.subcommand_matches("droplet") {
-    println!("{:?}", _api_key);
+  match matches.subcommand(){
+    ("droplet", Some(droplet_matches)) =>{
+      match droplet_matches.subcommand() {
+        ("list", Some(_list_droplet_matches)) =>{
+          list_droplet(&api_key);
+        },
+        ("create", Some(create_droplet_matches)) =>{
+          crete_droplet(&api_key);
+          println!("Pushing to {}", create_droplet_matches.value_of("cpu").unwrap());
+        },
+        ("delete", Some(_delete_droplet_matches)) =>{
+          delete_droplet(&api_key);
+        },
+        _ => unreachable!(),
+      }
+    },
+    ("loadbalancer", Some(lb_matches)) =>{
+      match lb_matches.subcommand() {
+        ("list", Some(_list_lb_matches)) =>{
+          list_lb(&api_key);
+        },
+        ("create", Some(_create_lb_matches)) =>{
+          create_lb(&api_key);
+        },
+        ("delete", Some(_delete_lbt_matches)) =>{
+          delete_lb(&api_key);
+        },
+        _ => unreachable!(),
+      }
+    },
+    ("", None)   => println!("No subcommand was used"),
+    _ => unreachable!(),
   }
+}
+
+fn list_droplet(key: &str) -> () {
+  println!("{}", key);
+}
+
+fn crete_droplet(key: &str) -> () {
+  println!("{}", key);
+}
+
+fn delete_droplet(key: &str) -> () {
+  println!("{}", key);
+}
+
+fn list_lb(key: &str) -> () {
+  println!("{}", key);
+}
+
+fn create_lb(key: &str) -> () {
+  println!("{}", key);
+}
+
+fn delete_lb(key: &str) -> () {
+  println!("{}", key);
 }
