784c4eda58a96537692f525b9a57aa2addbb620f
Eli Ma <eli@patch.sh>
2019-09-11 13:58:23 +0800

    Build with tide repository version
    Signed-off-by: Eli Ma <eli@patch.sh>

--- web/hello/src/main.rs
+++ web/hello/src/main.rs
@@ -1,7 +1,5 @@
-#![feature(async_await)]
-
-fn main() -> Result<(), std::io::Error> {
-    let mut app = tide::App::new(());
+fn main() {
+    let mut app = tide::App::new();
     app.at("/").get(|_| async move { "Hello, world!" });
-    app.serve("127.0.0.1:8080")
+    app.run("127.0.0.1:8000").unwrap();
 }
